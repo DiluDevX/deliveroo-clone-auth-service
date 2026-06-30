749e1993f92a87c1c590e6a67226ceb8b29c88c8
DiluDevX <dilumchandupa@gmail.com>
2026-06-30 17:34:10 +0530

    fix: remove unnecessary schema prefix from User table in migration files

--- prisma/migrations/20260105130911_auth/migration.sql
+++ prisma/migrations/20260105130911_auth/migration.sql
@@ -10,11 +10,11 @@
 -- AlterEnum
 BEGIN;
 CREATE TYPE "Role_new" AS ENUM ('user', 'platform_admin', 'restaurant_admin');
-ALTER TABLE "public"."User" ALTER COLUMN "role" DROP DEFAULT;
+ALTER TABLE "User" ALTER COLUMN "role" DROP DEFAULT;
 ALTER TABLE "User" ALTER COLUMN "role" TYPE "Role_new" USING (lower("role"::text)::"Role_new");
 ALTER TYPE "Role" RENAME TO "Role_old";
 ALTER TYPE "Role_new" RENAME TO "Role";
-DROP TYPE "public"."Role_old";
+DROP TYPE "Role_old";
 ALTER TABLE "User" ALTER COLUMN "role" SET DEFAULT 'user';
 COMMIT;
 
--- prisma/migrations/20260210135046_add_restaurant_user/migration.sql
+++ prisma/migrations/20260210135046_add_restaurant_user/migration.sql
@@ -12,11 +12,11 @@ CREATE TYPE "RestaurantRole" AS ENUM ('employee', 'super_admin', 'admin', 'finan
 -- AlterEnum
 BEGIN;
 CREATE TYPE "Role_new" AS ENUM ('user', 'platform_admin', 'restaurant_user');
-ALTER TABLE "public"."User" ALTER COLUMN "role" DROP DEFAULT;
+ALTER TABLE "User" ALTER COLUMN "role" DROP DEFAULT;
 ALTER TABLE "User" ALTER COLUMN "role" TYPE "Role_new" USING ("role"::text::"Role_new");
 ALTER TYPE "Role" RENAME TO "Role_old";
 ALTER TYPE "Role_new" RENAME TO "Role";
-DROP TYPE "public"."Role_old";
+DROP TYPE "Role_old";
 ALTER TABLE "User" ALTER COLUMN "role" SET DEFAULT 'user';
 COMMIT;
 
